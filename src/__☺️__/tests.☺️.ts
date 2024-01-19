f4d5ad14631273b26e54af3cf0066a1b52492ecd
Alex Andru <alex007d@gmail.com>
2024-01-19 22:45:31 +0100

    Add <li> tests

--- src/__☺️__/tests.☺️.ts
+++ src/__☺️__/tests.☺️.ts
@@ -3,38 +3,47 @@ import {
   compilerTest,
 } from "../lmaoTestRunner/lmaoTestRunner.js";
 
-// Hello World Test
+// TESTS TEST (for ctrl-f indexing)
+
+// Hello World
 const lmaoCode = "🤣🪬🫦Hello World💀🫦💀🪬💀🤣";
 const expectedHtml = `<html><body><h1>Hello World</h1></body></html>`;
 lmaoTestRunner("Hello World", () => compilerTest(lmaoCode, expectedHtml));
 
-// Div Test
+// Div
 const divLmao = "📦I am a div💀📦";
 const expectedDivHtml = "<div>I am a div</div>";
 lmaoTestRunner("Div", () => compilerTest(divLmao, expectedDivHtml));
 
-// Span Test
+// Span
 const spanLmao = "🤪I am a span💀🤪";
 const expectedSpanHtml = "<span>I am a span</span>";
 lmaoTestRunner("Span", () => compilerTest(spanLmao, expectedSpanHtml));
 
-// Paragraph Test
+// Paragraph
 const paragraphLmao = "💬I am a paragraph💀💬";
 const expectedParagraphHtml = "<p>I am a paragraph</p>";
 lmaoTestRunner("Paragraph", () =>
   compilerTest(paragraphLmao, expectedParagraphHtml)
 );
 
-// Ordered List Test
+// Ordered List
 const orderedListLmao = "📃Ordered List💀📃";
 const expectedOrderedListHtml = "<ol>Ordered List</ol>";
 lmaoTestRunner("Ordered List", () =>
   compilerTest(orderedListLmao, expectedOrderedListHtml)
 );
 
-// Unordered List Test
+// Unordered List
 const unorderedListLmao = "🎲Unordered List💀🎲";
 const expectedUnorderedListHtml = "<ul>Unordered List</ul>";
 lmaoTestRunner("Unordered List", () =>
   compilerTest(unorderedListLmao, expectedUnorderedListHtml)
+);
+
+// List Item
+const listItemLmao = "👂List Item💀👂";
+const expectedListItemHtml = "<li>List Item</li>";
+lmaoTestRunner("List Item", () =>
+  compilerTest(listItemLmao, expectedListItemHtml)
 );
--- src/tokenMap.ts
+++ src/tokenMap.ts
@@ -10,7 +10,7 @@ export const tokenMap: Record<string, TokenType> = {
   "💬": "PARAGRAPH",
   "📃": "ORDERED_LIST",
   "🎲": "UNORDERED_LIST",
-  "⏺️": "LIST_ITEM",
+  "👂": "LIST_ITEM",
   ERROR: "ERROR",
   TEXT: "TEXT",
 };
