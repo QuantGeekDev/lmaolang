04213db4a5c843a1fc1ae20707f1434ac586c8d6
Alex Andru <alex007d@gmail.com>
2024-01-19 18:20:38 +0100

    Add div test

--- src/__☺️__/helloWorld.test.ts
+++ src/__☺️__/helloWorld.test.ts
@@ -3,9 +3,12 @@ import {
   compilerTest,
 } from "../lmaoTestRunner/lmaoTestRunner.js";
 
+// Hello World Test
 const lmaoCode = "🤣🪬🫦Hello World💀🫦💀🪬💀🤣";
 const expectedHtml = `<html><body><h1>Hello World</h1></body></html>`;
+lmaoTestRunner("Hello World", () => compilerTest(lmaoCode, expectedHtml));
 
-lmaoTestRunner("🤣🪬🫦Hello World💀🫦💀🪬 💀🤣", () =>
-  compilerTest(lmaoCode, expectedHtml)
-);
+// Div Test
+const divLmao = "📦I am a div💀📦";
+const expectedDivHtml = "<div>I am a div</div>";
+lmaoTestRunner("Div Test", () => compilerTest(divLmao, expectedDivHtml));
