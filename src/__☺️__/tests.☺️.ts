760d4de07ac8d37c03b3868b60789d24ece1ca25
Alex Andru <alex007d@gmail.com>
2024-01-19 22:29:42 +0100

    Add <ol>,<ul> tests

--- src/__☺️__/tests.☺️.ts
+++ src/__☺️__/tests.☺️.ts
@@ -3,8 +3,6 @@ import {
   compilerTest,
 } from "../lmaoTestRunner/lmaoTestRunner.js";
 
-console.log();
-
 // Hello World Test
 const lmaoCode = "🤣🪬🫦Hello World💀🫦💀🪬💀🤣";
 const expectedHtml = `<html><body><h1>Hello World</h1></body></html>`;
@@ -13,16 +11,30 @@ lmaoTestRunner("Hello World", () => compilerTest(lmaoCode, expectedHtml));
 // Div Test
 const divLmao = "📦I am a div💀📦";
 const expectedDivHtml = "<div>I am a div</div>";
-lmaoTestRunner("Div Test", () => compilerTest(divLmao, expectedDivHtml));
+lmaoTestRunner("Div", () => compilerTest(divLmao, expectedDivHtml));
 
 // Span Test
 const spanLmao = "🤪I am a span💀🤪";
 const expectedSpanHtml = "<span>I am a span</span>";
-lmaoTestRunner("Span test", () => compilerTest(spanLmao, expectedSpanHtml));
+lmaoTestRunner("Span", () => compilerTest(spanLmao, expectedSpanHtml));
 
 // Paragraph Test
 const paragraphLmao = "💬I am a paragraph💀💬";
 const expectedParagraphHtml = "<p>I am a paragraph</p>";
-lmaoTestRunner("Paragraph test", () =>
+lmaoTestRunner("Paragraph", () =>
   compilerTest(paragraphLmao, expectedParagraphHtml)
+);
+
+// Ordered List Test
+const orderedListLmao = "📃Ordered List💀📃";
+const expectedOrderedListHtml = "<ol>Ordered List</ol>";
+lmaoTestRunner("Ordered List", () =>
+  compilerTest(orderedListLmao, expectedOrderedListHtml)
+);
+
+// Unordered List Test
+const unorderedListLmao = "🎲Unordered List💀🎲";
+const expectedUnorderedListHtml = "<ul>Unordered List</ul>";
+lmaoTestRunner("Unordered List", () =>
+  compilerTest(unorderedListLmao, expectedUnorderedListHtml)
 );
